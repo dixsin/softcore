02680a83901a0b8f2c487d495a32c51f2d349ecc
Dixsin <66322670+dixsin@users.noreply.github.com>
2022-01-03 17:36:28 +0300

    Update install.sh
    read this file

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,12 @@
-
+echo Установка ядра xSoftCore Public 1.0.0-BETA!
+wget --trust-server https://github.com/dixsin/softcore/tree/main/versions/src.tar.gz?raw=true
+tar -xvf src.tar.gz
+clear
+echo Установка библиотек 7.3!
+wget --trust-server https://github.com/dixsin/installer/blob/main/PHP-7.3_Linux-x86_64.tar.gz?raw=true
+tar -xvf PHP-7.3_Linux-x86_64.tar.gz
+clear
+echo Запуск сервера!
+chmod +x ./start.sh
+clear
+./start.sh
