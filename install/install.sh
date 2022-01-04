0ffac2968ee07b0a947cb0d0410e78054941676a
Dixsin <66322670+dixsin@users.noreply.github.com>
2022-01-04 17:44:21 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,5 @@
-echo Установка ядра xSoftCore Public 1.0.3!
-wget --trust-server https://github.com/dixsin/softcore/tree/main/versions/1.0.3p.tar.gz?raw=true
+echo Установка ядра xSoftCore Public 1.0.4!
+wget --trust-server https://github.com/dixsin/softcore/tree/main/versions/1.0.4p.tar.gz?raw=true
 tar -xvf 1.0.3p.tar.gz
 clear
 echo Установка библиотек 7.3!
